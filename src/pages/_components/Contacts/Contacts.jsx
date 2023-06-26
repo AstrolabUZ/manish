115a2ae2c31ffad3337f94c7552e7184cf86eccb
ZiyodillaDev <ziyodillazokirkhujayev@gmail.com>
2023-06-26 17:46:09 +0500

    commit

--- src/pages/_components/Contacts/Contacts.jsx
+++ src/pages/_components/Contacts/Contacts.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+import {
+  FaLocationDot,
+  FaSquarePhone,
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaTelegram,
+} from "react-icons/fa6";
+import { FaEnvelopeSquare } from "react-icons/fa";
 
 const Contacts = () => {
   return (
-    <section className="wrapper pt-20">
+    <section className="wrapper pt-20 px-12">
       <h2 className="text-center text-textColor text-[40px] font-semibold">
         Наши контакты
       </h2>
@@ -10,15 +18,15 @@ const Contacts = () => {
         Если вы заинтересованы в наших курсах, пожалуйста, оставьте свой номер
         телефона или свои данные, и мы свяжемся с вами в ближайшее время
       </p>
-      <div className="flex gap-32 pt-28 pb-24">
+      <div className="flex gap-32 pt-28 pb-24 px-12">
         <div className="w-full">
           <div className="flex gap-2 pb-7 items-center">
-            <p className="text-pColor font-semibold text-sm">Связаться</p>
+            <p className="text-pColor font-bold text-[15px]">Связаться</p>
             <hr className="w-full" />
           </div>
-          <ul>
-            <li className="list-disc text-pColor flex gap-3 pb-3">
-              <img />
+          <ul className="px-5">
+            <li className="list-disc list-outside  text-pColor flex items-center gap-3 pb-5">
+              <FaLocationDot />
               <a
                 className="text-[14px]"
                 href="https://www.google.com/maps?ll=41.333087,69.283945&z=15&t=m&hl=en-US&gl=US&mapclient=embed&cid=5745389386361951605"
@@ -26,14 +34,14 @@ const Contacts = () => {
                 г. Ташкент , ул. Минор 111
               </a>
             </li>
-            <li className="list-disc text-pColor flex gap-3 pb-3">
-              <img />
+            <li className="list-disc text-pColor flex gap-3 pb-5">
+              <FaSquarePhone />
               <a className="text-[14px]" href="tel:+998 93 395 23 32">
                 +998 93 395 23 32
               </a>
             </li>
-            <li className="list-disc text-pColor flex gap-3 pb-3">
-              <img />
+            <li className="list-disc text-pColor flex gap-3 pb-5">
+              <FaEnvelopeSquare />
               <a
                 className="text-[14px]"
                 href="mailto:manish.etiquette@gmail.com"
@@ -42,8 +50,8 @@ const Contacts = () => {
               </a>
             </li>
           </ul>
-          <div className="flex gap-2 pb-7 items-center">
-            <p className="text-pColor font-semibold text-sm w-20">Мы тут</p>
+          <div className="flex gap-2 pb-7 pt-14 items-center">
+            <p className="text-pColor font-bold text-[15px] w-20">Мы тут</p>
             <hr className="w-full" />
           </div>
           <iframe
@@ -53,60 +61,67 @@ const Contacts = () => {
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
           ></iframe>
-          <div className="flex gap-2 pt-10 pb-7 items-center">
-            <p className="text-pColor font-semibold text-sm w-72">
+          <div className="flex gap-2 pt-10 pb-5 items-center">
+            <p className="text-pColor font-bold text-[15px] w-96">
               Подписывайтесь на нас
             </p>
             <hr className="w-full" />
           </div>
-          <div className="flex pt-8 gap-4">
+          <div className="flex gap-4">
             <a
               className="rounded p-4 bg-iconBg hover:bg-iconHoverBg ease-in-out duration-300"
               href="#"
             >
-              <img />
+              <FaFacebook />
             </a>
             <a
               className="rounded p-4 bg-iconBg hover:bg-iconHoverBg ease-in-out duration-300"
               href="#"
             >
-              <img />
+              <FaInstagram />
             </a>
             <a
               className="rounded p-4 bg-iconBg hover:bg-iconHoverBg ease-in-out duration-300"
               href="#"
             >
-              <img />
+              <FaYoutube />
             </a>
             <a
               className="rounded p-4 bg-iconBg hover:bg-iconHoverBg ease-in-out duration-300"
               href="#"
             >
-              <img />
+              <FaTelegram />
             </a>
           </div>
         </div>
-        <div className="w-7/12 shadow-2xl px-10">
-          <h2 className="text-center w-full  text-textColor text-[32px] font-semibold pt-20">
+        <div className="w-9/12 shadow-2xl px-10">
+          <h2 className="text-center w-full  text-textColor text-[32px] font-semibold pt-12">
             Отправьте нам сообщение
           </h2>
           <p className="text-center text-sm pt-3 pb-8">
             Мы свяжемся с вами в ближайшее время.
           </p>
           <input
-            className="w-full border-solid border-2 border-iconHoverBg p-2 mb-5"
+            className="w-full border-solid border-2 border-iconHoverBg p-2 mb-5 focus:outline-none rounded-sm"
             type="text"
             placeholder="Ваше имя"
           />
           <input
-            className="w-full border-solid border-2 border-iconHoverBg p-2 mb-5"
+            className="w-full border-solid border-2 border-iconHoverBg p-2 mb-5 focus:outline-none rounded-sm"
             type="tel"
             placeholder="Номер телефона"
           />
-          <textarea className="border-solid border-2 w-full p-2 text-iconHoverBg focus:border-icon border-iconHoverBg" id="w3review" name="w3review" rows="6">
-          Сообщение
-          </textarea>
-          <button className="bg-buttonBg w-full py-3 rounded-3xl text-white text-sm font-semibold hover:bg-black ease-in-out duration-100" >Отправить</button>
+
+          <textarea
+            className="border-solid border-2 w-full p-2 text-iconHoverBg focus:border-icon border-iconHoverBg focus:outline-none focus:text-black rounded-sm"
+            id="w3review"
+            name="w3review"
+            rows="6"
+            placeholder="Сообщение"
+          ></textarea>
+          <button className="bg-buttonBg w-full py-3 rounded-3xl text-white text-sm font-semibold hover:bg-black ease-in-out duration-100 mb-24 mt-3 hover:scale-90">
+            Отправить
+          </button>
         </div>
       </div>
     </section>
